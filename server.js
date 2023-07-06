8afb089722b5e7a7fa3402110c5557dc4188bfcd
Fiifi Dawson <edem.fiifi.dawson@gmail.com>
2023-07-06 02:04:19 +0000

    New changes made

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
-// Desc: Main entry point for the application
+
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require('path');
 
-// Create express app
 const app = express({path:'config.env'});
 
-dotenvconfig()
+dotenv.config({path:'config.env'})
 const PORT = process.env.PORT || 8080;
 
-//morgan model
-// log requests
+// log requestd
 app.use(morgan('tiny'));
 
-// Parse requests of content-type - application/x-www-form-urlencoded
+// parse request to body-parser
+app.use(bodyparser.urlencoded({extended:true}))
+
+// set viw engine
+app.set("view engine", "ejs")
+// app.set("views".path.resolve(__dirname,"views/ejs"))
+
+// load assets
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")))
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")))
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")))
+
 app.get('/', (req, res) => {
-    res.send('CRUD APP');
+    res.render('index');
 });
 
-//
+
 app.listen(PORT, () => {console.log(`Server is running on http://localhost:${PORT}`)});
